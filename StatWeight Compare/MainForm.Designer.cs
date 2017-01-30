c7834a01ea6d6fd90a2a2757f5982bef5b17b005
George McClain <McClainG9616@Gmail.com>
2017-01-30 13:00:22 -0500

    stuff

---
+++ StatWeight Compare/MainForm.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace StatWeight_Compare
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbClass = new System.Windows.Forms.ComboBox();
+            this.cmbSubClass = new System.Windows.Forms.ComboBox();
+            this.lblClass = new System.Windows.Forms.Label();
+            this.lblSubClass = new System.Windows.Forms.Label();
+            this.cmbPreLoad1 = new System.Windows.Forms.ComboBox();
+            this.cmbPreLoad2 = new System.Windows.Forms.ComboBox();
+            this.txtValue1 = new System.Windows.Forms.TextBox();
+            this.txtValue3 = new System.Windows.Forms.TextBox();
+            this.txtValue2 = new System.Windows.Forms.TextBox();
+            this.txtValue22 = new System.Windows.Forms.TextBox();
+            this.txtValue23 = new System.Windows.Forms.TextBox();
+            this.txtValue21 = new System.Windows.Forms.TextBox();
+            this.lblPreLoad1 = new System.Windows.Forms.Label();
+            this.lblPreLoad2 = new System.Windows.Forms.Label();
+            this.lblValue1 = new System.Windows.Forms.Label();
+            this.lblValue2 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblValue21 = new System.Windows.Forms.Label();
+            this.lblValue22 = new System.Windows.Forms.Label();
+            this.lblValue23 = new System.Windows.Forms.Label();
+            this.btnWeigh = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtWeight1 = new System.Windows.Forms.TextBox();
+            this.txtWeight2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbClass
+            // 
+            this.cmbClass.FormattingEnabled = true;
+            this.cmbClass.Items.AddRange(new object[] {
+            "Class1",
+            "Class2",
+            "Class3",
+            "Class4"});
+            this.cmbClass.Location = new System.Drawing.Point(53, 14);
+            this.cmbClass.Name = "cmbClass";
+            this.cmbClass.Size = new System.Drawing.Size(121, 21);
+            this.cmbClass.TabIndex = 0;
+            // 
+            // cmbSubClass
+            // 
+            this.cmbSubClass.FormattingEnabled = true;
+            this.cmbSubClass.Items.AddRange(new object[] {
+            "SubClass1",
+            "SubClass2",
+            "SubClass3"});
+            this.cmbSubClass.Location = new System.Drawing.Point(268, 14);
+            this.cmbSubClass.Name = "cmbSubClass";
+            this.cmbSubClass.Size = new System.Drawing.Size(121, 21);
+            this.cmbSubClass.TabIndex = 1;
+            // 
+            // lblClass
+            // 
+            this.lblClass.AutoSize = true;
+            this.lblClass.Location = new System.Drawing.Point(12, 22);
+            this.lblClass.Name = "lblClass";
+            this.lblClass.Size = new System.Drawing.Size(35, 13);
+            this.lblClass.TabIndex = 2;
+            this.lblClass.Text = "Class:";
+            // 
+            // lblSubClass
+            // 
+            this.lblSubClass.AutoSize = true;
+            this.lblSubClass.Location = new System.Drawing.Point(205, 22);
+            this.lblSubClass.Name = "lblSubClass";
+            this.lblSubClass.Size = new System.Drawing.Size(57, 13);
+            this.lblSubClass.TabIndex = 3;
+            this.lblSubClass.Text = "Sub-Class:";
+            // 
+            // cmbPreLoad1
+            // 
+            this.cmbPreLoad1.FormattingEnabled = true;
+            this.cmbPreLoad1.Items.AddRange(new object[] {
+            "Pre-Load1",
+            "Pre-Load2"});
+            this.cmbPreLoad1.Location = new System.Drawing.Point(82, 129);
+            this.cmbPreLoad1.Name = "cmbPreLoad1";
+            this.cmbPreLoad1.Size = new System.Drawing.Size(121, 21);
+            this.cmbPreLoad1.TabIndex = 4;
+            this.cmbPreLoad1.SelectedIndexChanged += new System.EventHandler(this.cmbPreLoad1_SelectedIndexChanged);
+            // 
+            // cmbPreLoad2
+            // 
+            this.cmbPreLoad2.FormattingEnabled = true;
+            this.cmbPreLoad2.Items.AddRange(new object[] {
+            "Pre-Load1",
+            "Pre-Load2"});
+            this.cmbPreLoad2.Location = new System.Drawing.Point(378, 129);
+            this.cmbPreLoad2.Name = "cmbPreLoad2";
+            this.cmbPreLoad2.Size = new System.Drawing.Size(121, 21);
+            this.cmbPreLoad2.TabIndex = 5;
+            this.cmbPreLoad2.SelectedIndexChanged += new System.EventHandler(this.cmbPreLoad2_SelectedIndexChanged);
+            // 
+            // txtValue1
+            // 
+            this.txtValue1.Location = new System.Drawing.Point(82, 192);
+            this.txtValue1.Name = "txtValue1";
+            this.txtValue1.Size = new System.Drawing.Size(100, 20);
+            this.txtValue1.TabIndex = 6;
+            // 
+            // txtValue3
+            // 
+            this.txtValue3.Location = new System.Drawing.Point(82, 244);
+            this.txtValue3.Name = "txtValue3";
+            this.txtValue3.Size = new System.Drawing.Size(100, 20);
+            this.txtValue3.TabIndex = 7;
+            // 
+            // txtValue2
+            // 
+            this.txtValue2.Location = new System.Drawing.Point(82, 218);
+            this.txtValue2.Name = "txtValue2";
+            this.txtValue2.Size = new System.Drawing.Size(100, 20);
+            this.txtValue2.TabIndex = 8;
+            // 
+            // txtValue22
+            // 
+            this.txtValue22.Location = new System.Drawing.Point(378, 218);
+            this.txtValue22.Name = "txtValue22";
+            this.txtValue22.Size = new System.Drawing.Size(100, 20);
+            this.txtValue22.TabIndex = 11;
+            // 
+            // txtValue23
+            // 
+            this.txtValue23.Location = new System.Drawing.Point(378, 244);
+            this.txtValue23.Name = "txtValue23";
+            this.txtValue23.Size = new System.Drawing.Size(100, 20);
+            this.txtValue23.TabIndex = 10;
+            // 
+            // txtValue21
+            // 
+            this.txtValue21.Location = new System.Drawing.Point(378, 192);
+            this.txtValue21.Name = "txtValue21";
+            this.txtValue21.Size = new System.Drawing.Size(100, 20);
+            this.txtValue21.TabIndex = 9;
+            // 
+            // lblPreLoad1
+            // 
+            this.lblPreLoad1.AutoSize = true;
+            this.lblPreLoad1.Location = new System.Drawing.Point(20, 132);
+            this.lblPreLoad1.Name = "lblPreLoad1";
+            this.lblPreLoad1.Size = new System.Drawing.Size(56, 13);
+            this.lblPreLoad1.TabIndex = 12;
+            this.lblPreLoad1.Text = "Pre-Load1";
+            // 
+            // lblPreLoad2
+            // 
+            this.lblPreLoad2.AutoSize = true;
+            this.lblPreLoad2.Location = new System.Drawing.Point(316, 132);
+            this.lblPreLoad2.Name = "lblPreLoad2";
+            this.lblPreLoad2.Size = new System.Drawing.Size(56, 13);
+            this.lblPreLoad2.TabIndex = 13;
+            this.lblPreLoad2.Text = "Pre-Load2";
+            // 
+            // lblValue1
+            // 
+            this.lblValue1.AutoSize = true;
+            this.lblValue1.Location = new System.Drawing.Point(30, 195);
+            this.lblValue1.Name = "lblValue1";
+            this.lblValue1.Size = new System.Drawing.Size(46, 13);
+            this.lblValue1.TabIndex = 14;
+            this.lblValue1.Text = "Value1 :";
+            // 
+            // lblValue2
+            // 
+            this.lblValue2.AutoSize = true;
+            this.lblValue2.Location = new System.Drawing.Point(30, 221);
+            this.lblValue2.Name = "lblValue2";
+            this.lblValue2.Size = new System.Drawing.Size(46, 13);
+            this.lblValue2.TabIndex = 15;
+            this.lblValue2.Text = "Value2 :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 247);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Value3 :";
+            // 
+            // lblValue21
+            // 
+            this.lblValue21.AutoSize = true;
+            this.lblValue21.Location = new System.Drawing.Point(326, 195);
+            this.lblValue21.Name = "lblValue21";
+            this.lblValue21.Size = new System.Drawing.Size(46, 13);
+            this.lblValue21.TabIndex = 17;
+            this.lblValue21.Text = "Value1 :";
+            // 
+            // lblValue22
+            // 
+            this.lblValue22.AutoSize = true;
+            this.lblValue22.Location = new System.Drawing.Point(326, 221);
+            this.lblValue22.Name = "lblValue22";
+            this.lblValue22.Size = new System.Drawing.Size(46, 13);
+            this.lblValue22.TabIndex = 18;
+            this.lblValue22.Text = "Value2 :";
+            // 
+            // lblValue23
+            // 
+            this.lblValue23.AutoSize = true;
+            this.lblValue23.Location = new System.Drawing.Point(326, 247);
+            this.lblValue23.Name = "lblValue23";
+            this.lblValue23.Size = new System.Drawing.Size(46, 13);
+            this.lblValue23.TabIndex = 19;
+            this.lblValue23.Text = "Value3 :";
+            // 
+            // btnWeigh
+            // 
+            this.btnWeigh.Location = new System.Drawing.Point(222, 306);
+            this.btnWeigh.Name = "btnWeigh";
+            this.btnWeigh.Size = new System.Drawing.Size(75, 23);
+            this.btnWeigh.TabIndex = 20;
+            this.btnWeigh.Text = "Weigh";
+            this.btnWeigh.UseVisualStyleBackColor = true;
+            this.btnWeigh.Click += new System.EventHandler(this.btnWeigh_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(12, 335);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(537, 20);
+            this.txtOutput.TabIndex = 21;
+            // 
+            // txtWeight1
+            // 
+            this.txtWeight1.Location = new System.Drawing.Point(82, 284);
+            this.txtWeight1.Name = "txtWeight1";
+            this.txtWeight1.Size = new System.Drawing.Size(100, 20);
+            this.txtWeight1.TabIndex = 22;
+            // 
+            // txtWeight2
+            // 
+            this.txtWeight2.Location = new System.Drawing.Point(378, 284);
+            this.txtWeight2.Name = "txtWeight2";
+            this.txtWeight2.Size = new System.Drawing.Size(100, 20);
+            this.txtWeight2.TabIndex = 23;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 287);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Weight :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(326, 287);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Weight :";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 376);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtWeight2);
+            this.Controls.Add(this.txtWeight1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnWeigh);
+            this.Controls.Add(this.lblValue23);
+            this.Controls.Add(this.lblValue22);
+            this.Controls.Add(this.lblValue21);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblValue2);
+            this.Controls.Add(this.lblValue1);
+            this.Controls.Add(this.lblPreLoad2);
+            this.Controls.Add(this.lblPreLoad1);
+            this.Controls.Add(this.txtValue22);
+            this.Controls.Add(this.txtValue23);
+            this.Controls.Add(this.txtValue21);
+            this.Controls.Add(this.txtValue2);
+            this.Controls.Add(this.txtValue3);
+            this.Controls.Add(this.txtValue1);
+            this.Controls.Add(this.cmbPreLoad2);
+            this.Controls.Add(this.cmbPreLoad1);
+            this.Controls.Add(this.lblSubClass);
+            this.Controls.Add(this.lblClass);
+            this.Controls.Add(this.cmbSubClass);
+            this.Controls.Add(this.cmbClass);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbClass;
+        private System.Windows.Forms.ComboBox cmbSubClass;
+        private System.Windows.Forms.Label lblClass;
+        private System.Windows.Forms.Label lblSubClass;
+        private System.Windows.Forms.ComboBox cmbPreLoad1;
+        private System.Windows.Forms.ComboBox cmbPreLoad2;
+        private System.Windows.Forms.TextBox txtValue1;
+        private System.Windows.Forms.TextBox txtValue3;
+        private System.Windows.Forms.TextBox txtValue2;
+        private System.Windows.Forms.TextBox txtValue22;
+        private System.Windows.Forms.TextBox txtValue23;
+        private System.Windows.Forms.TextBox txtValue21;
+        private System.Windows.Forms.Label lblPreLoad1;
+        private System.Windows.Forms.Label lblPreLoad2;
+        private System.Windows.Forms.Label lblValue1;
+        private System.Windows.Forms.Label lblValue2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblValue21;
+        private System.Windows.Forms.Label lblValue22;
+        private System.Windows.Forms.Label lblValue23;
+        private System.Windows.Forms.Button btnWeigh;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtWeight1;
+        private System.Windows.Forms.TextBox txtWeight2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ StatWeight Compare/MainForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace StatWeight_Compare
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private float a = 0.86F;
+        private float b = 1.04F;
+        private float c = 0.60F;
+        private float d = 0.86F;
+        private float f = 1.00F;
+
+        private void cmbPreLoad1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbPreLoad1.Text == "Pre-Load1")
+            {
+                txtValue1.Text = ("380");
+                txtValue2.Text = ("1150");
+                txtValue3.Text = ("976");
+            }
+            if (cmbPreLoad1.Text == "Pre-Load2")
+            {
+                txtValue1.Text = ("450 ");
+                txtValue2.Text = ("970");
+                txtValue3.Text = ("976");
+            }
+        }
+
+        private void cmbPreLoad2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbPreLoad2.Text == "Pre-Load1")
+            {
+                txtValue21.Text = ("380");
+                txtValue22.Text = ("1150");
+                txtValue23.Text = ("976");
+            }
+            if (cmbPreLoad2.Text == "Pre-Load2")
+            {
+                txtValue21.Text = ("450 ");
+                txtValue22.Text = ("970");
+                txtValue23.Text = ("976");
+            }
+        }
+
+        private void btnWeigh_Click(object sender, EventArgs e)
+        {
+            Int32 val1 = Convert.ToUInt16(txtValue1.Text);
+            Int32 val2 = Convert.ToUInt16(txtValue2.Text);
+            Int32 val3 = Convert.ToUInt16(txtValue3.Text);
+
+            Int32 val21 = Convert.ToUInt16(txtValue21.Text);
+            Int32 val22 = Convert.ToUInt16(txtValue22.Text);
+            Int32 val23 = Convert.ToUInt16(txtValue23.Text);
+
+            float weight1Float1 = (b * val1) + (d * val2) + (f * val3);
+            txtWeight1.Text = Convert.ToString(weight1Float1);
+
+            float weight1Float2 = (a * val21) + (c * val22) + (f * val23);
+            txtWeight2.Text = Convert.ToString(weight1Float2);
+
+            if (weight1Float1 > weight1Float2)
+            {
+                txtOutput.Text = ("" + cmbPreLoad1.Text + " Weighs More than " + cmbPreLoad2.Text + "");
+            }
+
+            if (weight1Float1 < weight1Float2)
+            {
+                txtOutput.Text = ("" + cmbPreLoad2.Text + " Weighs More than " + cmbPreLoad1.Text + "");
+            }
+
+            if (weight1Float1 == weight1Float2)
+            {
+                txtOutput.Text = ("" + cmbPreLoad2.Text + " and " + cmbPreLoad1.Text + " Weigh the same!");
+            }
+        }
+    }
+}
